2b483f4ed1ac0f971cf32e335248fbdc0bbf9ba0
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-14 16:13:55 +0100

    map interface

---
+++ effectivegroovy/samples/src/eg07/01.groovy
@@ -0,0 +1,12 @@
+package eg07
+
+import groovy.swing.SwingBuilder
+import java.awt.event.MouseListener
+import javax.swing.JFrame
+
+def b
+new SwingBuilder().frame(size: [300, 300], show: true, defaultCloseOperation: JFrame.EXIT_ON_CLOSE) {
+    b = button(text: 'Click Me')
+}
+
+b.addMouseListener([ mouseClicked: { println 'clicked!' } ] as MouseListener)
