91695d11bb42982dedf454bc2587bc59d869c30f
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-14 09:01:10 +0100

    Join example

---
+++ effectivegroovy/samples/src/eg02/08.groovy
@@ -0,0 +1,15 @@
+package eg02
+
+List<Person> people = Person.findAll();
+
+String msg = "";
+for (Person p : people) {
+    msg = msg + p + "\n";
+}
+System.out.println(msg);
+
+
+
+
+
+println people.join('\n')
