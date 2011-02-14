f0b78fd1601f0ce3db00336118d754a84e4ec7d4
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-14 15:16:43 +0100

    finished overloading

--- effectivegroovy/samples/src/eg06/01.groovy
+++ effectivegroovy/samples/src/eg06/01.groovy
@@ -1,6 +1,19 @@
-package eg06
+package eg06;
 
-/**
- * 
- * @author Hamlet D'Arcy
- */
+public class CollectionClassifier {
+    public static String classify(Set s) { return "Set"; }
+    public static String classify(List l) { return "List"; }
+    public static String classify(Collection c) { return "Collection"; }
+
+    public static void main(String[] args) {
+        List<Collection> collections = Arrays.asList(
+                new HashSet(),
+                new ArrayList(),
+                new HashMap().values()
+        );
+
+        for (Collection c : collections) {
+            System.out.println(classify(c));
+        }
+    }
+}
