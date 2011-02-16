c0775a8d089da0b2a30a283cf0a7de7b52b1d767
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-17 00:03:43 +0100

    finished all preso except Grasp diagram

---
+++ presopatterns/samples/CdiTest/src/main/java/com/canoo/cdi/util/Images.java
@@ -0,0 +1,50 @@
+package com.canoo.cdi.util;
+
+
+public class Images {
+
+    public static String PING =
+            "MMMMMMMMMMMMMMMMMMMMMMWKxllox0NMMMMMMMMM           MMMMMMMMMW0xoloxKWMMMMMMMMMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMMMMMMMNko;'''',,,;lxXMMMMMM           MMMMMMXkl;,,,'''';oONMMMMMMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMMMMWOc''''''''',,,,;;cOWMMM           MMMWOc;;,,,,'''''''''cOWMMMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMMWx,''''''''''',,,,,,,,cXMM           MMXc,,,,,,,,''''''''''';xWMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMO;'''''''''''''',,,,,,,,;KM           MK;,,,,,,,,'''''''''''''';OMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMNl'..'''''''''''''',,,,,,,,:W           W:,,,,,,,,''''''''''''''..'lNMMMMMMMMMMM\n" +
+            "MMMMMMMMMMW:.....'''''''''''''',,,,,,,,k           k,,,,,,,,''''''''''''''.....:WMMMMMMMMMM\n" +
+            "MMMMMMMMMMx'.....'''''''''''''''',,,,,,o           o,,,,,,'''''''''''''''''....'xMMMMMMMMMM\n" +
+            "MMMMMMMMMMc'''.''''''''''''''''''',,,,,d           d,,,,,'''''''''''''''''''''''cMMMMMMMMMM\n" +
+            "MMMMMMMMMMk'''''''''''''''''''''''',,,;X Ping      X;,,,''''''''''''''''''''''''kMMMMMMMMMM\n" +
+            "MMMMMMMMMMWO;''''''''''''''''''''',,,,kM           Mk,,,,''''''''''''''''''''';OWMMMMMMMMMM\n" +
+            "MMMMMMMMMMMX0c''''''''''''''''''',,,;OMM           MMO;,,,'''''''''''''''''''c0XMMMMMMMMMMM\n" +
+            "MMMMMMMMMMXOO0o,'''''''''''''''',,,dNMMM           MMMNd,,,'''''''''''''''',o0OOXMMMMMMMMMM\n" +
+            "MMMMMMMMXOxk0K0x;''''''''''''''',oXMMMMM           MMMMMXo,''''''''''''''';x0K0kxOXMMMMMMMM\n" +
+            "MMMMMWKkdc;kxxOK0c'''''''''''':xXMMMMMMM           MMMMMMMXx:,'''''''''''c0KOxxk;cdkKWMMMMM\n" +
+            "MMMN0xo;,:dkKWMMMNO;'''''',:dKWMMMMMMMMM           MMMMMMMMMMKx:,'''''';ONMMMWKkd:,;ox0NMMM\n" +
+            "MXkxo,,cd0NMMMMMMMMWK0Ok0KNMMMMMMMMMMMMM           MMMMMMMMMMMMMNK0OO0XWMMMMMMMMN0dc,,oxkXM\n" +
+            "OdxxxoxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM           MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKxoxxxdO\n" +
+            "MOddkXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM           MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXkddOM\n" +
+            "MMXXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM           MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXXMM\n\n";
+
+    
+    public static String PONG =
+            "MMMMMMMMMMMMMMMMMMMMMMWKxllox0NMMMMMMMMM           MMMMMMMMMW0xoloxKWMMMMMMMMMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMMMMMMMNko;'''',,,;lxXMMMMMM           MMMMMMXkl;,,,'''';oONMMMMMMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMMMMWOc''''''''',,,,;;cOWMMM           MMMWOc;;,,,,'''''''''cOWMMMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMMWx,''''''''''',,,,,,,,cXMM           MMXc,,,,,,,,''''''''''';xWMMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMMMO;'''''''''''''',,,,,,,,;KM           MK;,,,,,,,,'''''''''''''';OMMMMMMMMMMMMM\n" +
+            "MMMMMMMMMMMNl'..'''''''''''''',,,,,,,,:W           W:,,,,,,,,''''''''''''''..'lNMMMMMMMMMMM\n" +
+            "MMMMMMMMMMW:.....'''''''''''''',,,,,,,,k           k,,,,,,,,''''''''''''''.....:WMMMMMMMMMM\n" +
+            "MMMMMMMMMMx'.....'''''''''''''''',,,,,,o           o,,,,,,'''''''''''''''''....'xMMMMMMMMMM\n" +
+            "MMMMMMMMMMc'''.''''''''''''''''''',,,,,d           d,,,,,'''''''''''''''''''''''cMMMMMMMMMM\n" +
+            "MMMMMMMMMMk'''''''''''''''''''''''',,,;X      Pong X;,,,''''''''''''''''''''''''kMMMMMMMMMM\n" +
+            "MMMMMMMMMMWO;''''''''''''''''''''',,,,kM           Mk,,,,''''''''''''''''''''';OWMMMMMMMMMM\n" +
+            "MMMMMMMMMMMX0c''''''''''''''''''',,,;OMM           MMO;,,,'''''''''''''''''''c0XMMMMMMMMMMM\n" +
+            "MMMMMMMMMMXOO0o,'''''''''''''''',,,dNMMM           MMMNd,,,'''''''''''''''',o0OOXMMMMMMMMMM\n" +
+            "MMMMMMMMXOxk0K0x;''''''''''''''',oXMMMMM           MMMMMXo,''''''''''''''';x0K0kxOXMMMMMMMM\n" +
+            "MMMMMWKkdc;kxxOK0c'''''''''''':xXMMMMMMM           MMMMMMMXx:,'''''''''''c0KOxxk;cdkKWMMMMM\n" +
+            "MMMN0xo;,:dkKWMMMNO;'''''',:dKWMMMMMMMMM           MMMMMMMMMMKx:,'''''';ONMMMWKkd:,;ox0NMMM\n" +
+            "MXkxo,,cd0NMMMMMMMMWK0Ok0KNMMMMMMMMMMMMM           MMMMMMMMMMMMMNK0OO0XWMMMMMMMMN0dc,,oxkXM\n" +
+            "OdxxxoxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM           MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKxoxxxdO\n" +
+            "MOddkXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM           MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXkddOM\n" +
+            "MMXXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM           MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXXMM\n\n";
+}
