3754254e0c8c8e23d7633bd864e27d212b57c291
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-17 17:17:03 +0100

    effective grovoy, small slide changes.

--- effectivegroovy/samples/src/ej08/01.groovy
+++ effectivegroovy/samples/src/ej08/01.groovy
@@ -2,8 +2,6 @@ package ej08
 
 import domain.Person
 
-// TODO: Generate equals, hashcode
-
 assert new Person(id: 1, firstName: 'John', lastName: 'Doe') ==
             new Person(id: 1, firstName: 'John', lastName: 'Doe')
 
