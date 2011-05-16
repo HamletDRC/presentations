ce8655258980e42d0764e3950d2acb701c41320f
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-05-16 18:40:19 +0200

    added

--- asttransformations/project/src/gcontracts/Ariane5.groovy
+++ asttransformations/project/src/gcontracts/Ariane5.groovy
@@ -2,8 +2,10 @@ package gcontracts
 
 import org.gcontracts.annotations.Ensures
 import org.gcontracts.annotations.Requires
+import org.gcontracts.annotations.Invariant
 
 
+@Invariant({ speed >= 0 })
 class Ariane5 {
 
     long speed = 0
