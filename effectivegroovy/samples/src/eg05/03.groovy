768b6071456839353531d30329e4b3b14cc80ef5
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-06-12 10:21:08 +0200

    effective groovy stuff from gr8

--- effectivegroovy/samples/src/eg05/03.groovy
+++ effectivegroovy/samples/src/eg05/03.groovy
@@ -1,6 +1,6 @@
 package eg05
 
-@Grab(group = 'org.codehaus.gpars', module = 'gpars', version = '0.11')
+@Grab(group = 'org.codehaus.gpars', module = 'gpars', version = '0.10')
 import static groovyx.gpars.dataflow.DataFlow.task
 import groovyx.gpars.dataflow.DataFlowVariable
 import domain.Person
