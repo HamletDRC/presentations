98cf0deaea325a01ca4ad65ec6a66c06f06cfcb6
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-06-24 22:30:36 -0400

    last minute groovy++ addition to codegen slides.

--- asttransformations/project/src/cofoja/Kettle.java
+++ asttransformations/project/src/cofoja/Kettle.java
@@ -4,27 +4,15 @@ import com.google.java.contract.Ensures;
 import com.google.java.contract.Invariant;
 import com.google.java.contract.Requires;
 
-@Invariant( {
-    " waterVolume >= 0",
-    " waterVolume <= maxVolume " })
 class Kettle {
 
     int waterVolume = 0;
     int maxVolume = 1000;
 
-    @Requires(" amount > 0 ")
-    @Ensures(" waterVolume == maxVolume || waterVolume > old(waterVolume) ")
     void addWater(int amount) {
         waterVolume = Math.min(maxVolume, amount + waterVolume);
     }
 
-    @Requires(" desiredAmount > 0 ")
-    @Ensures({
-            "result >= 0 ",
-            "(result == 0 " +
-                " ? waterVolume == old(waterVolume) " +
-                " : waterVolume < old(waterVolume)) "
-        })
     int pour(int desiredAmount) {
         int amountPoured = (desiredAmount <= waterVolume
                 ? desiredAmount
