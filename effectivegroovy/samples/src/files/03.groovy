5462e7d7ce6667961fd86f04bc9d1435c108bf50
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-07-07 15:04:48 +0200

    updated effective Groovy with new codenarc images and more more file example

--- effectivegroovy/samples/src/files/03.groovy
+++ effectivegroovy/samples/src/files/03.groovy
@@ -11,3 +11,23 @@ file.text = file.text + '\n // hello! '
 
 file.append '\n // better hello! '
 
+
+
+
+
+
+def list = ['one', 'two', 'three']
+for (def e : list) {
+    //file.append e   // this is bad
+}
+
+new File('./03output.txt').withWriter { Writer it ->
+    for (def e : list) {
+        it.append "$e\n"
+    }
+}
+
+ // hello! 
+ // better hello! 
+ // hello! 
+ // better hello! 
