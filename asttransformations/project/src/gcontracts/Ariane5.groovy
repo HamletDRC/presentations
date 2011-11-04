02dad78b8a67d7484a286199e65a6ccf2eb829d3
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-11-04 15:55:05 +0100

    baseline

--- asttransformations/project/src/gcontracts/Ariane5.groovy
+++ asttransformations/project/src/gcontracts/Ariane5.groovy
@@ -11,16 +11,16 @@ class Ariane5 {
     long speed = 0
     boolean engineStarted = false
 
-    @Requires({ increase > 0 && engineStarted == true })
-    @Ensures({ old.speed < speed })
-    void accelerate(increase) {
-        speed += increase
-    }
-
     @Requires({ engineStarted == false })
     @Ensures( { engineStarted == true } )
     void startEngine() {
         engineStarted = true
+    }
+
+    @Requires({ increase > 0 && engineStarted == true })
+    @Ensures({ old.speed < speed })
+    void accelerate(increase) {
+        speed += increase
     }
 }
 
