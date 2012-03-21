afe15c19687b4f45d585eedeed9c1608ce8584d6
Hamlet D'Arcy <hamletdrc@gmail.com>
2012-03-21 07:47:25 +0100

    removed example

--- staticanalysis/project/src/findbugs/Example2.java
+++ staticanalysis/project/src/findbugs/Example2.java
@@ -2,14 +2,7 @@ package findbugs;
 
 public class Example2 {
 
-	private Object state;
-
-	private synchronized void setState(Object state) {
-		this.state = state;
-	}
-
-	@Override
-	public String toString() {
-		return "Example2{ state=" + state + '}';
+	boolean isOdd(int input) {
+		return input % 2 == 1;
 	}
 }
